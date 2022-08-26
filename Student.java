7328acbd749d16d666511f337066c3e6530f225e
Sathish <58475636+sathish2000k@users.noreply.github.com>
2022-08-26 23:16:17 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,17 @@
+package schoolManagementApp;
+
+class Student {
+	String name;
+	String DOB;
+	String gender;
+	String phoneNumber;
+	String city;
+	Student(String name,String DOB,String gender,String phoneNumber, String city){
+		this.name = name;
+		this.DOB = DOB;
+		this.gender = gender;
+		this.phoneNumber = phoneNumber;
+		this.city = city;
+	}
+
+}
